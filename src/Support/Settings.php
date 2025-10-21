d306ef3330915017604223a3355e7cd7fb3d85e6
Matthew Ryder <matthew.r@klaritymonitoring.com>
2025-10-21 23:50:26 +0000

    tweaks for settings plugin

--- src/Support/Settings.php
+++ src/Support/Settings.php
@@ -2,8 +2,6 @@
 
 namespace Klarity\LibremsTest\Support;
 
-use App\Models\User;
-// 👇 add Hooks\
 use LibreNMS\Interfaces\Plugins\Hooks\SettingsHook;
 
 class Settings implements SettingsHook
@@ -15,9 +13,12 @@ class Settings implements SettingsHook
         return $this->view;
     }
 
-    public function authorize($user): bool
+    // ⬇️ remove the App\Models\User typehint (and be defensive)
+    public function authorize($user = null): bool
     {
-        return method_exists($user, 'can') ? $user->can('admin') : true;
+        return is_object($user) && method_exists($user, 'can')
+            ? $user->can('admin')
+            : true; // allow if no user was passed
     }
 
     public function data(array $settings = []): array
