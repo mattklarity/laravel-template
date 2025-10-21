1d5f111da20ee1c219e7fb1c19fd9e9c3f3a462f
Matthew Ryder <matthew.r@klaritymonitoring.com>
2025-10-21 23:13:43 +0000

    tweak settings

--- src/Support/Settings.php
+++ src/Support/Settings.php
@@ -3,23 +3,21 @@
 namespace Klarity\LibremsTest\Support;
 
 use App\Models\User;
-use LibreNMS\Interfaces\Plugins\SettingsHook;
+// 👇 add Hooks\
+use LibreNMS\Interfaces\Plugins\Hooks\SettingsHook;
 
 class Settings implements SettingsHook
 {
-    // The Blade view to render for your settings UI
     public function view(): string
     {
         return 'librems-test::settings';
     }
 
-    // Who can see/save settings
     public function authorize(User $user): bool
     {
         return $user->can('admin');
     }
 
-    // Extra data to pass into the view
     public function data(array $settings = []): array
     {
         return ['settings' => $settings];
